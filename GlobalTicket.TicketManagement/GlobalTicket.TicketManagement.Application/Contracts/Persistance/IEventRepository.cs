834fbdf7d5af3de012dcafd14bf2eb980755c96f
dali2579 <ma.benamor@gmail.com>
2021-02-04 10:32:46 +0100

    Add a generic repository with a repository for each domain entity contracts to

---
+++ GlobalTicket.TicketManagement/GlobalTicket.TicketManagement.Application/Contracts/Persistance/IEventRepository.cs
@@ -0,0 +1,11 @@
+﻿using GlobalTicket.TicketManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GlobalTicket.TicketManagement.Application.Contracts.Persistance
+{
+    public interface IEventRepository:IAsyncRepository<Event>
+    {
+    }
+}
