a101070c0ca2381efac8611f58143fb70f8d7a92
dali2579 <ma.benamor@gmail.com>
2021-02-04 09:47:39 +0100

    Add Domain project to the core layer withn a .Net standard project.

---
+++ GlobalTicket.TicketManagement/GlobalTicket.TicketManagement.Domain/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using GlobalTicket.TicketManagement.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+
+namespace GlobalTicket.TicketManagement.Domain.Entities
+{
+    public class Category : AuditableEntity
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Event> Events { get; set; }
+    }
+}
---
+++ GlobalTicket.TicketManagement/GlobalTicket.TicketManagement.Domain/Entities/Common/AuditableEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GlobalTicket.TicketManagement.Domain.Entities.Common
+{
+    public class AuditableEntity
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string LastModifiedby { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
